35206d6d6a44b9af1939820e611a1b8ba08d8cc1
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-19 23:15:11 +0200

    Added  Dadi steps

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('JS OK')
+/* 
+ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato */
+
+/* 
+1. Recuperare l'elemento d'interesse all'interno del DOM.
+
+2. Generare un Nr. ramdom per la cpu e inserirlo in console.
+
+3. Generare un Nr. ramdom per il giocatore e inserirlo in console.
+
+4. Confrontare i due risultati ottenuti e indicare chi è il vincitore 
+   in base a chi ha ottenuto il Nr più alto.
+
+5. Stampare in pagina i due risultati e il nome del vincitore.
+*/
